1f93ec09d32db3a09badce107d5b17ebc3648b38
Thiago Nebuloni <100470956+thiagonebuloni@users.noreply.github.com>
2023-11-23 21:49:55 -0300

    Time: 37 ms (70.40%) | Memory: 16.3 MB (36.81%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -0,0 +1,9 @@
+import string
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        alphabet: list[str] = string.ascii_lowercase
+        for letter in alphabet:
+            if letter not in sentence:
+                return False
+        return True
