3f33362cd03ea988a70945d36cf02e74d8adccc1
Thiago Nebuloni <100470956+thiagonebuloni@users.noreply.github.com>
2023-12-11 08:31:22 -0300

    Time: 111 ms (79.50%) | Memory: 16.9 MB (23.51%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,15 +1,4 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        dict_names = dict()
-        for i in range(len(names)):
-            for j in range(len(heights) - 1):
-                if heights[i] > heights[j]:
-                    tmp_height = heights[i]
-                    heights[i] = heights[j]
-                    heights[j] = tmp_height
-
-                    tmp_names = names[i]
-                    names[i] = names[j]
-                    names[j] = tmp_names
-
-        return names
+        x=sorted(zip(heights,names), reverse=True)
+        return [b for a, b in x]
